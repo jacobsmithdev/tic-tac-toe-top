fb643a6c43770c5b612573e640eaf068a66155d2
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2025-02-08 11:08:57 -0700

    Add function to check gameboard columns for winner
    Check whether any column on the gameboard contains the same element, and
    ensure it is not null (otherise a column of nulls would count as a win).

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 const gameboard = (function() {
+    const BOARD_SIZE = 3;
+
     // Board formatted as [row][col]
     const board = [
         [null, null, null],
@@ -27,6 +29,18 @@ const gameboard = (function() {
         return board.some(row => {
             const allItemsMatch = row.every(item => item === row[0]);
             return allItemsMatch && !row.includes(null);
+        });
+    };
+
+    const colHasWinner = function() {
+        const firstRow = board[0];
+        return firstRow.some((firstItem, col) => {
+            let allItemsMatch = true;
+            for (row = 0; row < BOARD_SIZE; row++) {
+                const item = board[row][col];
+                if (item !== firstItem) allItemsMatch = false;
+            }
+            return firstItem !== null && allItemsMatch;
         });
     };
 
